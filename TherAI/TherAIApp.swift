b9af78d00495a3db22de1849db9ecf8f2105e63e
Stephan Gazarov <sgzrov@gmail.com>
2025-08-29 15:13:27 -0700

    Initial commit from Xcode

---
+++ TherAI/TherAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TherAIApp.swift
+//  TherAI
+//
+//  Created by Stephan  on 29.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct TherAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
