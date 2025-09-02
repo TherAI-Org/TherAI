133faf826685d9aac5cb0b1898d413a8ea1935f8
Stephan Gazarov <sgzrov@gmail.com>
2025-09-01 20:11:20 -0700

    feat: add ProfileStats model to track user session statistics and ratings

---
+++ TherAI/Profile/Models/ProfileStats.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+struct ProfileStats {
+    let totalSessions: Int
+    let newSessions: Int
+    let averageRating: Double
+    let sessionsResolved: Int
+    let sessionsNeedsImprovement: Int
+}
+
+
+
