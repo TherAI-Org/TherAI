84559126cb9d391f2541fc8eb4aa83030313b09f
Stephan Gazarov <sgzrov@gmail.com>
2025-09-01 20:13:27 -0700

    feat: add Notification extension for chat session and message events

---
+++ TherAI/Shared/Notifications.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension Notification.Name {
+    static let chatSessionCreated = Notification.Name("chat.session.created")
+    static let chatMessageSent = Notification.Name("chat.message.sent")
+}
+
+
