61d5c0dd508cd4f8a8fb42feddf03ab29fd9f678
Stephan Gazarov <sgzrov@gmail.com>
2025-09-07 16:24:07 -0700

    feat: implement MessagesListView for displaying chat messages in a scrollable format

---
+++ TherAI/Chat/Views/Components/MessagesListView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct MessagesListView: View {
+    let messages: [ChatMessage]
+
+    var body: some View {
+        ScrollViewReader { proxy in
+            ScrollView {
+                LazyVStack(spacing: 12) {
+                    ForEach(messages) { message in
+                        MessageBubble(message: message)
+                    }
+                }
+                .padding()
+            }
+            .onChange(of: messages.count) {
+                if let lastMessage = messages.last {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        proxy.scrollTo(lastMessage.id, anchor: .bottom)
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MessagesListView(messages: [
+        ChatMessage(content: "Hello!", isFromUser: true),
+        ChatMessage(content: "Hi there!", isFromUser: false)
+    ])
+}
