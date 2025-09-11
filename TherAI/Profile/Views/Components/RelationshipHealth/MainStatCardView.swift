a66a0eebf55b0ba068b56492486c045d7a7a3b4a
mmarufov <marufov254@gmail.com>
2025-09-10 18:08:55 -0700

    style: update MainStatCardView icon foreground color for improved visual consistency

--- TherAI/Profile/Views/Components/RelationshipHealth/MainStatCardView.swift
+++ TherAI/Profile/Views/Components/RelationshipHealth/MainStatCardView.swift
@@ -48,7 +48,7 @@ struct MainStatCardView: View {
 
                         Image(systemName: isExpanded ? "chevron.up" : "chevron.down")
                             .font(.system(size: 16, weight: .medium))
-                            .foregroundColor(.secondary)
+                            .foregroundColor(Color(red: 0.4, green: 0.2, blue: 0.6))
                             .opacity(0.7)
                             .rotationEffect(.degrees(isExpanded ? 180 : 0))
                             .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0.1), value: isExpanded)
