20e8683c857bc26b102fcc2761fc483cf836d37b
mmarufov <marufov254@gmail.com>
2025-09-12 02:00:07 -0700

    feat: add PreferenceKeys enum for managing user preferences related to appearance, haptics, and auto-scroll

---
+++ TherAI/Shared/PreferenceKeys.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+enum PreferenceKeys {
+    static let appearancePreference = "appearance_preference"
+    static let hapticsEnabled = "haptics_enabled"
+    static let autoScrollEnabled = "auto_scroll_enabled"
+}
